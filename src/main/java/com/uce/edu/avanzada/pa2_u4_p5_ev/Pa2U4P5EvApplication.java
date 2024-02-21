75a2c2f4c60ab7f79b54297048124d967cba9e4d
Emil2245 <emilmontiel@hotmail.com>
2024-02-20 20:36:35 -0500

    first commit

---
+++ src/main/java/com/uce/edu/avanzada/pa2_u4_p5_ev/Pa2U4P5EvApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.avanzada.pa2_u4_p5_ev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P5EvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P5EvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/avanzada/pa2_u4_p5_ev/Pa2U4P5EvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.avanzada.pa2_u4_p5_ev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U4P5EvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
